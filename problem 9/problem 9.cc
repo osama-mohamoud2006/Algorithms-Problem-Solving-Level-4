db0dce48e701d4bfef9ae1a372c2cd811e5a7bc8
osama mohamoud <wwwhdosama@gmail.com>
2025-09-01 21:29:05 +0300

    Auto commit: 2025-09-01 21:29:05

--- problem 9/problem 9.cc
+++ problem 9/problem 9.cc
@@ -6,20 +6,13 @@
 using namespace std;
 using namespace AllStuff;
 
+// to check the feb
 bool isLeap(short y)
 {
-    return ((y % 400 == 0) || (y % 100 != 0 && y % 4 == 0));
+    return ((y % 400 == 0) || (y % 4 == 0 && y % 100 != 0));
 }
 
-short NumberOfDaysInMonth(short y, short m)
-{
-
-    if (m < 1 || 12 < m)
-        return 0;
-    short arr[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    return (m == 2) ? ((isLeap(y) == true) ? 29 : 28) : arr[m];
-}
-
+// return day order starting from 0 (sun)
 short DayOrder(short year, short month, short day)
 {
     short a = ((14 - month) / 12);
@@ -29,12 +22,24 @@ short DayOrder(short year, short month, short day)
     return (day + y + ((y / 4)) - ((y / 100)) + ((y / 400)) + (((31 * m) / 12))) % 7;
 }
 
+// to return the string that indicating the month name
 string monthName(short m)
 {
     string month[] = {"jan", "feb", "mar", "apr", "may", "jun", "jul", "agu", "sep", "oct", "nov", "dec"};
     return month[m - 1];
 }
 
+// return the number of days in month
+short NumberOfDaysInMonth(short y, short m)
+{
+
+    if (m < 1 || 12 < m)
+        return 0;
+    short arr[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    return (m == 2) ? ((isLeap(y) == true) ? 29 : 28) : arr[m];
+}
+
+// print the month
 void printMonthCalnder(short m, short y)
 {
     int theLastDayInMonth = NumberOfDaysInMonth(y, m); // the last day
@@ -58,11 +63,17 @@ void printMonthCalnder(short m, short y)
     printf("\n");
 }
 
-
-int main()
+void PrintYearcalnder(short y)
 {
-    short m = enter_postive_number("\nenter m: ");
-    short y = enter_postive_number("\nenter y: ");
+    for (short i = 1; i <= 12; i++)
+    {
+        printMonthCalnder(i,y); // print the month
+        cout<<endl;
+    }
+    
+}
 
-    printMonthCalnder(m, y);
+int main(){
+     short y = enter_postive_number("\nenter y: ");
+    PrintYearcalnder(y);
 }

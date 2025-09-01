59652adf9ea256bbe191d752897d06bcce45faa7
osama mohamoud <wwwhdosama@gmail.com>
2025-09-01 16:47:28 +0300

    Auto commit: 2025-09-01 16:47:28

--- problem 5/problem 5.cc
+++ problem 5/problem 5.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-#include"E:\projects\my library\AllStuff.h"  
+#include "E:\projects\my library\AllStuff.h"
 using namespace AllStuff;
 using namespace std;
 
@@ -8,41 +8,46 @@ bool isLeap(short y)
     return ((y % 400 == 0) || (y % 4 == 0 && y % 100 != 0));
 }
 
-short numberOfDaysInMonth(short y, short m )
+short numberOfDaysInMonth(short y, short m)
 {
-    //month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+    // month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
     if (m == 2)
         return (isLeap(y) == true) ? 29 : 28;
-    else if(m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12) return 31;
-    else return 30;
-        
+
+    // else if(m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12) return 31;
+
+    short arrIs31D[7] = {1, 3, 5, 7, 8, 10, 12};
+
+    for(short n: arrIs31D){
+        if(n==m) return 30;
+    }
+
+
+    return 30;
 }
 
-int hoursInMonth(short y)
+int hoursInMonth(short y, short m)
 {
-    return 24 * numberOfDaysInMonth(y,m);
+    return 24 * numberOfDaysInMonth(y, m);
 }
 
-int MinInMonth(short y)
+int MinInMonth(short y, short m)
 {
-    return 60 * hoursInMonth(y,m);
+    return 60 * hoursInMonth(y, m);
 }
 
-int secondsInMonth(short y)
+int secondsInMonth(short y, short m)
 {
-    return MinInMonth(y,m) * 60;
+    return MinInMonth(y, m) * 60;
 }
 
 int main()
 {
-    short y= enter_postive_number("enter y: ");
+    short y = enter_postive_number("enter y: ");
     short m = enter_postive_number("\nenter m: ");
 
-    cout<<"the number of days in month is: "<<numberOfDaysInMonth(y,m)<<endl;
-     cout<<"the number of hours in month is: "<<hoursInMonth(y,m)<<endl;
-     cout<<"the number of min in month is: "<<MinInMonth(y,m)<<endl;
-     cout<<"the number of seconds is: "<<secondsInMonth(y,m)<<endl;
-
-
-
+    cout << "the number of days in month is: " << numberOfDaysInMonth(y, m) << endl;
+    cout << "the number of hours in month is: " << hoursInMonth(y, m) << endl;
+    cout << "the number of min in month is: " << MinInMonth(y, m) << endl;
+    cout << "the number of seconds is: " << secondsInMonth(y, m) << endl;
 }

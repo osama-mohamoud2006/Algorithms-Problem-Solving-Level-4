4e47ccb918cde345209a590c9d04ce8bbc198bc1
osama mohamoud <wwwhdosama@gmail.com>
2025-09-01 15:30:24 +0300

    Auto commit: 2025-09-01 15:30:24

--- problem 4/problem 4.cc
+++ problem 4/problem 4.cc
@@ -15,32 +15,28 @@ short numberOFDays(short y)
     return (leapYearORNot(y) == true) ? 366 : 365;
 }
 
-int numberOfHours(short d)
+int numberOfHours(short y)
 {
-    return 24 * d;
+    return 24 * numberOFDays(y);
 }
 
-int numberOfMin(int hour)
+int numberOfMin(int y)
 {
-    return hour * 60;
+    return numberOfHours(y) * 60;
 }
 
-double numberOfSeconds(int min)
+int numberOfSeconds(int y)
 {
-    return min*60;
+    return numberOfMin(y) * 60;
 }
 
 int main()
 {
     short y = enter_postive_number("enter y: ");
-    short numOfdays = numberOFDays(y);
-    int numOfhours = numberOfHours(numOfdays);
-    int numOfmin = numberOfMin(numOfhours);
-    double numOfSeconds=numberOfSeconds(numOfmin);
 
-    cout << "number of days in year (" << y << ") is " << numOfdays << endl;
-    cout << "number of hours in year (" << y << ") is " << numOfhours << endl;
+    cout << "number of days in year (" << y << ") is " << numberOFDays(y) << endl;
+    cout << "number of hours in year (" << y << ") is " << numberOfHours(y) << endl;
 
-    cout << "number of min in year (" << y << ") is " << numOfmin << endl;
-        cout << "number of seconds in year (" << y << ") is " << numOfSeconds << endl;
+    cout << "number of min in year (" << y << ") is " << numberOfMin(y) << endl;
+    cout << "number of seconds in year (" << y << ") is " << numberOfSeconds(y) << endl;
 }

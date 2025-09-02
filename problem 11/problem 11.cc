abfc740998aa1340765123bfad5bce9208395f7b
osama mohamoud <wwwhdosama@gmail.com>
2025-09-02 16:15:38 +0300

    Auto commit: 2025-09-02 16:15:38

---
+++ problem 11/problem 11.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <stdio.h>
+#include "E:\projects\my library\AllStuff.h"
+using namespace std;
+using namespace AllStuff;
+
+bool isLeap(short y)
+{
+    return ((y % 400 == 0) || (y % 4 == 0 && y % 100 != 0));
+}
+
+short NumberOfDaysInMonth(short y, short m)
+{
+
+    if (m < 1 || 12 < m)
+        return 0;
+    short arr[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    return (m == 2) ? ((isLeap(y) == true) ? 29 : 28) : arr[m];
+}
+
+short DayUntillDate(short y, short m, short d)
+{
+    short totalDays = 0;
+
+    for (short i = 1; i <= m - 1; i++)
+    {
+        totalDays += NumberOfDaysInMonth(y, i);
+    }
+    return totalDays + d;
+}
+
+
+
+int main()
+{
+     short d = enter_postive_number("\nenter d: ");
+    short m = enter_postive_number("\nenter m: ");
+    short y = enter_postive_number("\nenter y: ");
+    cout<<"\nNumber of days from begining of the year is: "<<DayUntillDate(y,m,d)<<endl;
+}

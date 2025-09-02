a2c9d71e0f4440fb8157d99c82baca827a85a5ed
osama mohamoud <wwwhdosama@gmail.com>
2025-09-02 16:30:21 +0300

    Auto commit: 2025-09-02 16:30:20

--- Debugging P11/Debugging P11.cpp
+++ Debugging P11/Debugging P11.cpp
@@ -1,20 +1,67 @@
-// Debugging P11.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <stdio.h>
+#include "E:\projects\my library\AllStuff.h"
+using namespace std;
+using namespace AllStuff;
+
+bool isLeap(short y)
+{
+    return ((y % 400 == 0) || (y % 4 == 0 && y % 100 != 0));
+}
+
+short NumberOfDaysInMonth(short y, short m)
+{
+
+    if (m < 1 || 12 < m)
+        return 0;
+    short arr[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    return (m == 2) ? ((isLeap(y) == true) ? 29 : 28) : arr[m];
+}
+
+short DayUntillDate(short y, short m, short d)
+{
+    short totalDays = 0;
+
+    for (short i = 1; i <= m - 1; i++)
+    {
+        totalDays += NumberOfDaysInMonth(y, i);
+    }
+    return totalDays + d;
+}
+
+struct stdate { short y = 0;  short m = 0; short d = 0; };
+
+stdate TheDateAgain(short DayUntillDate, short y) {
+    stdate data;
+    data.y = y;// the year
+
+    short RemainingDays = DayUntillDate; // the function that count number of days starting from 1/1 untill your data
+    short m = 1; // month
+    while (true) {
+        short monthDays = NumberOfDaysInMonth(y, m); // will return the number of days in month
+
+        if (RemainingDays > monthDays) {
+            RemainingDays -= monthDays; // will decrease the the remaing days untill reach the day
+            m++;
+        }
+        else{
+            data.d = RemainingDays; 
+            data.m= m;
+            break;
+        }
+    }
+    return data;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    short d = enter_postive_number("\nenter d: ");
+    short m = enter_postive_number("\nenter m: ");
+    short y = enter_postive_number("\nenter y: ");
+    cout << "\nNumber of days from begining of the year is: " << DayUntillDate(y, m, d) << endl;
+
+    stdate date = TheDateAgain(DayUntillDate(y, m, d), y);
+    cout << date.d << "/" << date.m << "/" << date.y << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

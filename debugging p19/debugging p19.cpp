9b104a57b8c33c7057ddb21c565b1c84b832c0b0
osama mohamoud <wwwhdosama@gmail.com>
2025-09-03 14:22:17 +0300

    Auto commit: 2025-09-03 14:22:16

---
+++ debugging p19/debugging p19.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <stdio.h>
+#include "E:\projects\my library\AllStuff.h"
+using namespace std;
+using namespace AllStuff;
+
+struct stdate
+{
+    short y = 0;
+    short m = 0;
+    short d = 0;
+};
+
+stdate FillDate()
+{
+
+    stdate d;
+    static short c = 1;
+    cout << "\ndate" << c << endl;
+    d.d = enter_postive_number("enter d: ");
+    d.m = enter_postive_number("\nenter m: ");
+    d.y = enter_postive_number("\nenter y: ");
+    c++;
+
+    return d;
+}
+
+bool isLeap(short y)
+{
+    return ((y % 400 == 0) || (y % 4 == 0 && y % 100 != 0));
+}
+
+bool isDate1LessThanDate2(stdate date, stdate date2)
+{
+    return (date.y < date2.y) ? true : (date.y == date2.y) ? (date.m < date2.m) ? true : (date.m == date2.m) ? (date.d < date2.d) ? true : false
+        : false
+        : false;
+}
+
+bool Is2DatesAreEqual(stdate date1, stdate date2)
+{
+    return (date1.y == date2.y && date1.m == date2.m && date1.d == date2.d);
+}
+
+short NumberOfDaysInMonth(short y, short m)
+{
+
+    if (m < 1 || 12 < m)
+        return 0;
+    short arr[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    return (m == 2) ? ((isLeap(y) == true) ? 29 : 28) : arr[m];
+}
+
+bool isLastDayInMonth(stdate date)
+{
+    return (NumberOfDaysInMonth(date.y, date.m) == date.d);
+}
+
+bool isLastMonthInYear(stdate date)
+{
+    return (date.m == 12);
+}
+
+bool isFirstMonthInYear(stdate date)
+{
+    return (date.m == 1);
+}
+
+stdate dateAfterAddingOneDay(stdate date)
+{
+
+    if (isLastDayInMonth(date) == true)
+    {
+        date.d = 1; // rest day 1
+
+        if (isLastMonthInYear(date) == true) // 1/1
+        {
+            date.m = 1;
+            date.y = date.y + 1;
+        }
+        else
+            date.m++;
+    }
+    else
+    {
+        date.d = date.d + 1;
+    }
+    return date;
+}
+
+stdate dateAfterMinusOneDay(stdate date)
+{
+    if (date.d == 1) // 1/1/2025
+    {
+        if (isFirstMonthInYear(date)) // 1
+        {
+            date.m = 12;
+            date.y -= 1;
+        }
+        // 31/
+        if(isFirstMonthInYear(date)!=true)date.m -= 1;
+        date.d = NumberOfDaysInMonth(date.y, date.m); // from 1 -->31
+    }
+    else
+    {
+        date.d -= 1;
+    }
+    return date;
+}
+
+int diffBetween2days(stdate date1, stdate date2, bool endDay = false)
+{
+    int days = 0;
+    while (true)
+    {
+        if (isDate1LessThanDate2(date1, date2) != true)
+        { 
+            days--;
+            date1 = dateAfterMinusOneDay(date1);
+           
+            if (Is2DatesAreEqual(date1, date2))
+                break;
+        }
+        days++;
+        date1 = dateAfterAddingOneDay(date1);
+        if (Is2DatesAreEqual(date1, date2))
+            break;
+    }
+    return (endDay == false) ? days : ++days;
+}
+
+int main()
+{
+    stdate date1 = FillDate();
+    stdate date2 = FillDate();
+    cout << "diff between 2 dates: " << diffBetween2days(date1, date2) << " day" << endl;
+}

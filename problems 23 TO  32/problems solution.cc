4b964c8ba504ae6ffaabb07053793734a22b608f
osama mohamoud <wwwhdosama@gmail.com>
2025-09-03 17:39:40 +0300

    Auto commit: 2025-09-03 17:39:39

--- problems 23 TO  32/problems solution.cc
+++ problems 23 TO  32/problems solution.cc
@@ -7,7 +7,6 @@
 using namespace std;
 using namespace AllStuff;
 
-
 // add one day
 struct stdate
 {
@@ -42,11 +41,6 @@ bool isDate1LessThanDate2(stdate date, stdate date2)
                                                            : false;
 }
 
-bool Is2DatesAreEqual(stdate date1, stdate date2)
-{
-    return (date1.y == date2.y && date1.m == date2.m && date1.d == date2.d);
-}
-
 short NumberOfDaysInMonth(short y, short m)
 {
 
@@ -66,7 +60,85 @@ bool isLastMonthInYear(stdate date)
     return (date.m == 12);
 }
 
-bool isFirstMonthInYear(stdate date)
+stdate dateAfterAddingOneDay(stdate date)
 {
-    return (date.m == 1);
+
+    if (isLastDayInMonth(date) == true)
+    {
+        date.d = 1; // rest day 1
+
+        if (isLastMonthInYear(date) == true) // 1/1
+        {
+            date.m = 1;
+            date.y = date.y + 1;
+        }
+        else
+            date.m++;
+    }
+    else
+    {
+        date.d = date.d + 1;
+    }
+    return date;
+}
+
+stdate AddXdays(short days, stdate date)
+{
+    for (int i = 1; i <= days; i++)
+    {
+        date = dateAfterAddingOneDay(date);
+    }
+    return date;
+}
+
+stdate AddOneWeek(stdate date)
+{
+    if (isLastDayInMonth(date))
+    { //  30/1/2021
+
+        date.d = 7;
+        date.m++; // 7/2
+
+        if (isLastMonthInYear(date))
+        {               // 31/12 -- >> 7/1
+            date.m = 1; // month
+            date.d = 7; // day
+        }
+    }
+
+    else if (NumberOfDaysInMonth(date.y, date.m) != 30) // 2 ,4
+    {
+        date.d = 6;
+        date.m++;
+    }
+    else
+    {
+        if (isLastMonthInYear(date))
+        {
+            date.d = 6;
+            date.m = 1;
+            date.y++;
+        }
+        date.d += 7;
+    }
+
+    return date;
+}
+
+string print_date(stdate date)
+{
+    return (to_string(date.d) + "/" + to_string(date.m) + "/" + to_string(date.y));
+}
+
+int main()
+{
+    stdate date = FillDate();
+    //short days = enter_postive_number("enter d to add: "); // to enter days to add
+
+    date = dateAfterAddingOneDay(date);
+    cout << "01 Adding one day is: " << print_date(date) << endl;
+    //date = AddXdays(days, date);
+   // cout << "02 Adding 10 days is: " << print_date(date) << endl;
+    date = AddOneWeek(date);
+    cout<<"03 Adding one week is: "<< print_date(date) << endl;
 }

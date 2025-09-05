e17f8ce7dfc942fa2439faacf4483f0108fd3ad6
osama mohamoud <wwwhdosama@gmail.com>
2025-09-05 16:36:17 +0300

    Auto commit: 2025-09-05 16:36:17

--- problem 54/problem 54.cc
+++ problem 54/problem 54.cc
@@ -3,7 +3,7 @@
 #include <ctime>
 #include "E:\projects\my library\AllStuff.h"
 using namespace std;
-using namespace AllStuff;   
+using namespace AllStuff;
 
 struct stdate
 {
@@ -17,6 +17,40 @@ bool isLeap(short y)
     return ((y % 400 == 0) || (y % 4 == 0 && y % 100 != 0));
 }
 
+short NumberOfDaysInMonth(short y, short m)
+{
+
+    if (m < 1 || 12 < m)
+        return 0;
+    short arr[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    return (m == 2) ? ((isLeap(y) == true) ? 29 : 28) : arr[m];
+}
+
+bool isLastDayInMonth(stdate date)
+{
+    return (NumberOfDaysInMonth(date.y, date.m) == date.d);
+}
+
+bool isLastMonthInYear(stdate date)
+{
+    return (date.m == 12);
+}
+
+short DayOrder(short year, short month, short day)
+{
+    short a = ((14 - month) / 12);
+    short y = year - a;
+    short m = month + 12 * a - 2;
+
+    return (day + y + ((y / 4)) - ((y / 100)) + ((y / 400)) + (((31 * m) / 12))) % 7;
+}
+
+short DayOrder(stdate date)
+{
+    return DayOrder(date.y, date.m, date.d);
+}
+
+
 bool isDate1LessThanDate2(stdate date, stdate date2)
 {
     return (date.y < date2.y) ? true : (date.y == date2.y) ? (date.m < date2.m) ? true : (date.m == date2.m) ? (date.d < date2.d) ? true : false
@@ -24,4 +58,40 @@ bool isDate1LessThanDate2(stdate date, stdate date2)
                                                            : false;
 }
 
+bool isItWeekEnd(stdate date)
+{
+    // i suppose fri is the week end
+    return (DayOrder(date) == 5) ? true : false;
+}
 
+stdate dateAfterAddingOneDay(stdate date)
+{
+
+    if (isLastDayInMonth(date) == true)
+    {
+        date.d = 1; // rest day 1
+
+        if (isLastMonthInYear(date) == true) // 1/1
+        {
+            date.m = 1;
+            date.y = date.y + 1;
+        }
+        else
+            date.m++;
+    }
+    else
+    {
+        date.d = date.d + 1;
+    }
+    return date;
+}
+
+int DiffBetween2Dates(stdate date1, stdate date2)
+{
+    int days = 0;
+    while (isDate1LessThanDate2(date1, date2))
+    {
+        days++;
+date1=dateAfterAddingOneDay(date1);
+    }
+}
